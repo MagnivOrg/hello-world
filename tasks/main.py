d2b4b0718af55cba632d29dda238b0113966481c
Jared Zoneraich <jszoneraich@gmail.com>
2022-07-13 13:55:13 -0400

    switch to daily

--- tasks/main.py
+++ tasks/main.py
@@ -1,7 +1,7 @@
 from magniv.core import task
 from datetime import datetime
 
-@task(schedule="@hourly")
+@task(schedule="@daily")
 def hello_world():
 	print("Hello world, the time is {}".format(datetime.now()))
 
